d667b16fe7ede266f4b2321cd344ed2d77951a79
RachidLakrimi <145000371+Rachid847@users.noreply.github.com>
2024-06-03 16:22:26 +0200

    SidebarProfesseur

---
+++ thedanselab_manager/components/SidebarProfesseur/index.tsx
@@ -0,0 +1,28 @@
+
+"use client";
+import { FaHome, FaUser, FaCog } from 'react-icons/fa';
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+
+export function SidebarProfesseur() {
+  return (
+    <Sidebar aria-label="Default sidebar example">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          
+          <Sidebar.Item href="#" icon={FaHome}>
+            Home
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={FaUser} >
+            Creation élève
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={FaCog} >
+            Mes cours
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
+
+export default SidebarProfesseur;
