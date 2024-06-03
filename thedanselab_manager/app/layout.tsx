454ad2ba6cf707e6dc8aa53b344b05cc53c6db1a
Nitronix24 <antonin.ledee@student.junia.com>
2024-06-03 10:05:12 +0200

    add navbar without style

--- thedanselab_manager/app/layout.tsx
+++ thedanselab_manager/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavbarVisiteur from "@/components/NavbarVisiteur";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="fr" data-theme="light">
       <body className={inter.className}>
         <div className="flex-wrap">
+          <NavbarVisiteur></NavbarVisiteur>
           <div className="flex">{children}</div>
         </div>
       </body>
