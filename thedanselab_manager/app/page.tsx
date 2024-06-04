3c9f79a1692ffc898a52e1d7760fd6015a271163
RachidLakrimi <145000371+Rachid847@users.noreply.github.com>
2024-06-04 10:50:37 +0200

    SidebarEleve

--- thedanselab_manager/app/page.tsx
+++ thedanselab_manager/app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import React, { useState } from "react";
+
 // import { supabase } from "../supabaseClient";
 
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation"
+import SidebarEleve from "@/components/SidebarEleve";
 
 const Home: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen w-full">
       <h1>Home</h1>
+      <SidebarEleve/>
     </div>
   );
 };
