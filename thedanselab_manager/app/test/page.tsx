3fff0d2a69f483fca669e21e5fb5cc5f61f9058c
Nitronix24 <antonin.ledee@student.junia.com>
2024-06-04 14:53:52 +0200

    add test page

---
+++ thedanselab_manager/app/test/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React, { useState } from "react";
+
+const test: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-full"></div>
+  );
+};
+
+export default test;
