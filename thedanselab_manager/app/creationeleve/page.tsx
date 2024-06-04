61db25a9a8e1cfe105a475fc3520b24afa8ce9f0
RachidLakrimi <145000371+Rachid847@users.noreply.github.com>
2024-06-04 15:08:29 +0200

    Update page.tsx

--- thedanselab_manager/app/creationeleve/page.tsx
+++ thedanselab_manager/app/creationeleve/page.tsx
@@ -30,15 +30,14 @@ const creationeleve: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen w-full">
       <div className="max-w-sm w-full p-8 bg-white rounded-lg shadow-md">
-        <h1 className="text-center text-2xl mb-6">Créer un compte</h1>
-
+        <h1 className="text-center text-2xl mb-6">Créer un compte élève</h1>
         <form onSubmit={handleSubmit} className="flex flex-col gap-5">
           <NameInput name={name} setName={setName} />
           <FirstNameInput FirstName={firstName} setFirstName={setFirstName} />
           <EmailInput email={email} setEmail={setEmail} />
           <PasswordInput password={password} setPassword={setPassword} />
           {error && <p className="text-red-500">{error}</p>}
-          <ValidationButton text="Inscription" />
+          <ValidationButton text="Inscription élève" />
         </form>
       </div>
     </div>
