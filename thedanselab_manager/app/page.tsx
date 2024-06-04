22b1114de21a2f02c37ecb27d8446d0fff6b7e6b
RachidLakrimi <145000371+Rachid847@users.noreply.github.com>
2024-06-04 10:39:26 +0200

    SidebarAdmin

--- thedanselab_manager/app/page.tsx
+++ thedanselab_manager/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import SidebarAdmin from "@/components/SidebarAdmin"
 // import { supabase } from "../supabaseClient";
 
 import { useRouter } from "next/navigation";
@@ -8,6 +9,7 @@ const Home: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen w-full">
       <h1>Home</h1>
+      
     </div>
   );
 };
