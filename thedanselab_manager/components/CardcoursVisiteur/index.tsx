a53be8913c796ab76016bc8a5d2f2e12e8a37c9c
RachidLakrimi <145000371+Rachid847@users.noreply.github.com>
2024-06-05 10:54:05 +0200

    CardcoursVisiteur

--- thedanselab_manager/components/CardcoursVisiteur/index.tsx
+++ thedanselab_manager/components/CardcoursVisiteur/index.tsx
@@ -4,34 +4,40 @@ import { Card } from "flowbite-react";
 type CardImageProps = {
   titre: string;
   description: string;
-  image: string;
+  image?: string; // Image est maintenant optionnel
   prix: string;
-  date: string; // Ajout de la date
-  heure: string; // Ajout de l'heure
-  duree: string; // Ajout de la durée
+  date: Date; 
+  heure: string; 
+  duree: string; 
 };
 
-const CardImage: React.FC<CardImageProps> = ({ titre, description, image, prix, date, heure, duree }) => {
+const CardcoursVisiteur: React.FC<CardImageProps> = ({ titre, description, image, prix, date, heure, duree }) => {
   return (
-    <Card
-      className="max-w-sm"
-      imgAlt={titre}
-      imgSrc={image}
-    >
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        {titre}
-      </h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        {description}
-      </p>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        {prix}
-      </p>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        Date: {date}, Heure: {heure}, Durée: {duree}
-      </p>
+    <Card className="max-w-sm">
+      {image && (
+        <img
+          className="w-full h-48 object-cover"
+          src={image}
+          alt={titre}
+        />
+      )}
+      <div className="p-4">
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {titre}
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {description}
+        </p>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {prix}
+        </p>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Date: {date ? date.toLocaleDateString() : "Date non disponible"}, Heure: {heure}, Durée: {duree}
+        </p>
+      </div>
     </Card>
   );
 };
 
-export default CardImage;
+export default CardcoursVisiteur;
+
