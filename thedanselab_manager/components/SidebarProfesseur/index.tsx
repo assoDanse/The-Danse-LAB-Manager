b6040a503705fc82e8888b84fe56cb03474ad3e3
Nitronix24 <antonin.ledee@student.junia.com>
2024-06-05 11:29:47 +0200

    Update index.tsx

--- thedanselab_manager/components/SidebarProfesseur/index.tsx
+++ thedanselab_manager/components/SidebarProfesseur/index.tsx
@@ -1,23 +1,29 @@
-
 "use client";
-import { FaHome, FaUser, FaCog } from 'react-icons/fa';
+import { FaHome, FaUser, FaCog } from "react-icons/fa";
 import { Sidebar } from "flowbite-react";
-import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+import {
+  HiArrowSmRight,
+  HiChartPie,
+  HiInbox,
+  HiShoppingBag,
+  HiTable,
+  HiUser,
+  HiViewBoards,
+} from "react-icons/hi";
 
 export function SidebarProfesseur() {
   return (
     <Sidebar aria-label="Default sidebar example">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          
           <Sidebar.Item href="#" icon={FaHome}>
             Home
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={FaUser} >
-            Creation élève
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={FaCog} >
+          <Sidebar.Item href="#" icon={FaUser}>
             Mes cours
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={FaCog}>
+            Creation élève
           </Sidebar.Item>
         </Sidebar.ItemGroup>
       </Sidebar.Items>
