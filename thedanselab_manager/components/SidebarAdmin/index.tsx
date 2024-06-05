794c5507ac2c1eae3d295f06872be21839d25506
Nitronix24 <antonin.ledee@student.junia.com>
2024-06-05 11:38:53 +0200

    Update index.tsx

--- thedanselab_manager/components/SidebarAdmin/index.tsx
+++ thedanselab_manager/components/SidebarAdmin/index.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { Sidebar } from "flowbite-react";
-import { HiHome, HiBookOpen, HiUserAdd, HiUserGroup, HiCreditCard, HiCurrencyDollar } from "react-icons/hi";
+import {
+  HiHome,
+  HiBookOpen,
+  HiUserAdd,
+  HiUserGroup,
+  HiCreditCard,
+  HiCurrencyDollar,
+} from "react-icons/hi";
 
 export function SidebarAdmin() {
   return (
