b47b6ef66eb472ea1243e5ff5ad9201509d845a3
YannFonie <145002247+YannFonie@users.noreply.github.com>
2024-06-06 15:51:19 +0200

    Update page.tsx

--- thedanselab_manager/app/test/page.tsx
+++ thedanselab_manager/app/test/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import CardCoursVisiteur from "@/components/CardCoursVisiteur";
+import DialogueBoxInput from "@/components/DialogueBoxinput";
 import React, { useState } from "react";
 
 const test: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen w-full"></div>
+    <div className="flex justify-center items-center h-screen w-full"><DialogueBoxInput></DialogueBoxInput></div>
   );
 };
 
