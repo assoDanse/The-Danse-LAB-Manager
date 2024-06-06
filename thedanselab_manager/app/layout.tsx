bd446ba3dc5178b825da9a0ae9d15ed850393ef7
Nitronix24 <antonin.ledee@student.junia.com>
2024-06-06 14:31:19 +0200

    navbar responsive 2

--- thedanselab_manager/app/layout.tsx
+++ thedanselab_manager/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavbarVisiteur from "@/components/NavbarVisiteur";
+import NavbarVisiteur from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
