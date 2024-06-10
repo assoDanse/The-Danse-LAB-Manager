abdf2534a5e6bd08faecf98724e7f41b48969b4e
Nitronix24 <antonin.ledee@student.junia.com>
2024-06-10 11:04:32 +0200

    add blank pages
    - add /eleve/cours
    - add /eleve/tarifs
    - add /professeur/cours
    - remove /admin/createCours

---
+++ thedanselab_manager/app/user/eleve/tarifs/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React, { useState } from "react";
+
+const TarifsEleve: React.FC = () => {
+  return <div className="flex justify-center items-center w-full">content</div>;
+};
+
+export default TarifsEleve;
