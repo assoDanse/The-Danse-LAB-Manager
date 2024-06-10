986923b910a87ad553b97b430d989d5d693a6967
Nitronix24 <antonin.ledee@student.junia.com>
2024-06-10 11:11:14 +0200

    add multiple blank pages
    - add blank reset password page
    - add blank edit contact page

---
+++ thedanselab_manager/app/auth/reset_password/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React, { useState } from "react";
+
+const ResetPassword: React.FC = () => {
+  return <div className="flex justify-center items-center w-full">content</div>;
+};
+
+export default ResetPassword;
