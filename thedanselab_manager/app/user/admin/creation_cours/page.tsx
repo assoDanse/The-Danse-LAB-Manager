464a891bb77c6ef8a109bb9bb60eb1e72cd487f4
Nitronix24 <antonin.ledee@student.junia.com>
2024-06-10 10:30:19 +0200

    update createCours

--- thedanselab_manager/app/user/admin/creation_cours/page.tsx
+++ thedanselab_manager/app/user/admin/creation_cours/page.tsx
@@ -10,20 +10,40 @@ import DurationInput from "@/components/DurationInput";
 import PriceInput from "@/components/PriceInput";
 import LinkListInput from "@/components/linkListInput";
 
-const Home: React.FC = () => {
+const CreateCours: React.FC = () => {
+  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!email || !password || !name || !firstName) {
+      setError("Veuillez remplir tous les champs.");
+      return;
+    }
+
+    setError("");
+  };
+
   return (
     <div className="flex flex-col justify-center items-center h-screen w-full space-y-4">
       <h1>Creation Cours</h1>
-      <TitleInput />
-      <DescriptionInput />
-      <ProfesseurInput />
-      <TypeDeCoursInput />
-      <PriceInput />
-      <DateInput /> {/* Ici */}
-      <DurationInput />
-      <LinkListInput />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+        <TitleInput />
+        <DescriptionInput />
+        <ProfesseurInput />
+        <TypeDeCoursInput />
+        <PriceInput />
+        <DateInput /> {/* Ici */}
+        <DurationInput />
+        <LinkListInput />
+      </form>
     </div>
   );
 };
 
-export default Home;
+export default CreateCours;
