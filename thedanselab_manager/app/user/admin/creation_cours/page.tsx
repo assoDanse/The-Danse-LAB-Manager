09d2a1eba078903da343ca54ce6520cf7c8d3e80
Nitronix24 <antonin.ledee@student.junia.com>
2024-06-13 09:50:34 +0200

    add import

--- thedanselab_manager/app/user/admin/creation_cours/page.tsx
+++ thedanselab_manager/app/user/admin/creation_cours/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import ProfesseurInput from "@/components/professeurInput";
 import TypeDeCoursInput from "@/components/TypeDeCoursInput";
 import TitleInput from "@/components/TiltleInput";
