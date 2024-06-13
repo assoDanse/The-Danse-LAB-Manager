ec8384bfe2127aaa38f4e79b51dba9e39678a236
Nitronix24 <antonin.ledee@student.junia.com>
2024-06-13 09:58:34 +0200

    fix v2

--- thedanselab_manager/app/user/visiteur/contact/page.tsx
+++ thedanselab_manager/app/user/visiteur/contact/page.tsx
@@ -13,11 +13,14 @@ const Contact: React.FC = () => {
   useEffect(() => {
     const fetchContactInfo = async () => {
       try {
-        const response = await fetch('/api/contact');
+        const response = await fetch("/api/contact");
         const data = await response.json();
         setContactInfo(data);
       } catch (error) {
-        console.error("Erreur lors de la récupération des informations de contact:", error);
+        console.error(
+          "Erreur lors de la récupération des informations de contact:",
+          error
+        );
       }
     };
 
@@ -25,7 +28,7 @@ const Contact: React.FC = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-full bg-gray-100">
+    <div className="flex flex-col items-center justify-center w-full bg-gray-100">
       <div className="w-full max-w-md p-8 bg-white rounded shadow-md">
         <h2 className="text-3xl font-bold mb-6 text-center">Contactez-nous</h2>
         {contactInfo ? (
@@ -40,7 +43,9 @@ const Contact: React.FC = () => {
             </div>
           </>
         ) : (
-          <p className="text-center font-bold">Chargement des informations...</p>
+          <p className="text-center font-bold">
+            Chargement des informations...
+          </p>
         )}
       </div>
     </div>
