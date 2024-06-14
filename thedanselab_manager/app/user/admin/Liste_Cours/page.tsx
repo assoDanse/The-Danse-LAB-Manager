33ff5501e65adddf077ecfe89efef72f78b0966c
YannFonie <145002247+YannFonie@users.noreply.github.com>
2024-06-14 15:17:58 +0200

    Create page.tsx

---
+++ thedanselab_manager/app/user/admin/Liste_Cours/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import React from "react";
+import CourseTable from "@/components/CourseTable";
+
+
+const Liste_Cours: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center w-full p-8">
+      <div className="max-w-5xl w-full">
+        <h1 className="text-2xl font-bold mb-6">Liste des Cours</h1>
+        <CourseTable />
+      </div>
+    </div>
+  );
+};
+
+export default Liste_Cours;
