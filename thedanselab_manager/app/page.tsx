1e23ccb4f348e30ab868c8991a3c32c449d831e1
Nitronix24 <antonin.ledee@student.junia.com>
2024-06-14 11:57:21 +0200

    fix index.tsx

--- thedanselab_manager/app/page.tsx
+++ thedanselab_manager/app/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import TitleInput from "@/components/TiltleInput";
+
 const Home: React.FC = () => {
   return (
     <div className="flex justify-center items-center w-full">
-      <div>
-        <TitleInput/>
-      </div>
+      <div>Accueil</div>
     </div>
   );
 };
