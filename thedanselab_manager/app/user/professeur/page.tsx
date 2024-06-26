cc69e5ed0728adf7a80c2dc4bedb30561e264805
Nitronix24 <antonin.ledee@student.junia.com>
2024-06-26 12:34:23 +0200

    fix page professeur

--- thedanselab_manager/app/user/professeur/page.tsx
+++ thedanselab_manager/app/user/professeur/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import React, { useState } from "react";
 
-
 const panelprof: React.FC = () => {
   return <div className="flex justify-center items-center w-full">content</div>;
 };
 
 export default panelprof;
-q
